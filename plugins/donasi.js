1f35e25e199964f6e901a9d52372785191a9526c
Irfanff99 <107552437+Irfanff99@users.noreply.github.com>
2022-07-13 09:02:33 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,8 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
+│ •  [0858-5585-4993]
+│─「 Dana • Ovo • Gopay 」
+│ •  [0895-1049-7533]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
